5f5072ebcd560845cee69b5ccbf54035324d1305
ZhaoJie <ZhaoJieLoveJingChunyan@hotmail.com>
2015-07-30 02:04:18 +0800

    Complete most codes of the Game River main frame

--- ZjSmallGameCollection/GameRiver.Designer.cs
+++ ZjSmallGameCollection/GameRiver.Designer.cs
@@ -28,18 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameRiver));
             this.mainMain = new System.Windows.Forms.MenuStrip();
             this.gameMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.startNewItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.panel1 = new System.Windows.Forms.Panel();
             this.messageList = new System.Windows.Forms.ListBox();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.statueBar = new System.Windows.Forms.ToolStripStatusLabel();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.gameBack = new System.Windows.Forms.PictureBox();
+            this.chess1 = new System.Windows.Forms.PictureBox();
+            this.chess2 = new System.Windows.Forms.PictureBox();
+            this.chess3 = new System.Windows.Forms.PictureBox();
+            this.chess4 = new System.Windows.Forms.PictureBox();
+            this.chess8 = new System.Windows.Forms.PictureBox();
+            this.chess7 = new System.Windows.Forms.PictureBox();
+            this.chess6 = new System.Windows.Forms.PictureBox();
+            this.chess5 = new System.Windows.Forms.PictureBox();
             this.mainMain.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess5)).BeginInit();
             this.SuspendLayout();
             // 
             // mainMain
@@ -65,23 +83,16 @@
             // 
             this.startNewItem.Name = "startNewItem";
             this.startNewItem.ShortcutKeys = System.Windows.Forms.Keys.F2;
-            this.startNewItem.Size = new System.Drawing.Size(152, 22);
+            this.startNewItem.Size = new System.Drawing.Size(121, 22);
             this.startNewItem.Text = "开局";
             this.startNewItem.Click += new System.EventHandler(this.gameMenuItem_ChildClick);
             // 
             // exitItem
             // 
             this.exitItem.Name = "exitItem";
-            this.exitItem.Size = new System.Drawing.Size(152, 22);
+            this.exitItem.Size = new System.Drawing.Size(121, 22);
             this.exitItem.Text = "退出";
             this.exitItem.Click += new System.EventHandler(this.gameMenuItem_ChildClick);
-            // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(147, 28);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(180, 180);
-            this.panel1.TabIndex = 1;
             // 
             // messageList
             // 
@@ -126,27 +137,145 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "我方信息";
             // 
+            // gameBack
+            // 
+            this.gameBack.BackColor = System.Drawing.Color.White;
+            this.gameBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.gameBack.Image = ((System.Drawing.Image)(resources.GetObject("gameBack.Image")));
+            this.gameBack.Location = new System.Drawing.Point(147, 30);
+            this.gameBack.Name = "gameBack";
+            this.gameBack.Size = new System.Drawing.Size(180, 180);
+            this.gameBack.TabIndex = 6;
+            this.gameBack.TabStop = false;
+            // 
+            // chess1
+            // 
+            this.chess1.BackColor = System.Drawing.Color.White;
+            this.chess1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("chess1.BackgroundImage")));
+            this.chess1.Image = ((System.Drawing.Image)(resources.GetObject("chess1.Image")));
+            this.chess1.Location = new System.Drawing.Point(151, 30);
+            this.chess1.Name = "chess1";
+            this.chess1.Size = new System.Drawing.Size(48, 48);
+            this.chess1.TabIndex = 7;
+            this.chess1.TabStop = false;
+            // 
+            // chess2
+            // 
+            this.chess2.BackColor = System.Drawing.Color.White;
+            this.chess2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("chess2.BackgroundImage")));
+            this.chess2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chess2.Image = ((System.Drawing.Image)(resources.GetObject("chess2.Image")));
+            this.chess2.Location = new System.Drawing.Point(213, 30);
+            this.chess2.Name = "chess2";
+            this.chess2.Size = new System.Drawing.Size(48, 48);
+            this.chess2.TabIndex = 8;
+            this.chess2.TabStop = false;
+            // 
+            // chess3
+            // 
+            this.chess3.BackColor = System.Drawing.Color.White;
+            this.chess3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("chess3.BackgroundImage")));
+            this.chess3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chess3.Location = new System.Drawing.Point(276, 30);
+            this.chess3.Name = "chess3";
+            this.chess3.Size = new System.Drawing.Size(48, 48);
+            this.chess3.TabIndex = 9;
+            this.chess3.TabStop = false;
+            // 
+            // chess4
+            // 
+            this.chess4.BackColor = System.Drawing.Color.White;
+            this.chess4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("chess4.BackgroundImage")));
+            this.chess4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chess4.Location = new System.Drawing.Point(278, 95);
+            this.chess4.Name = "chess4";
+            this.chess4.Size = new System.Drawing.Size(48, 48);
+            this.chess4.TabIndex = 10;
+            this.chess4.TabStop = false;
+            // 
+            // chess8
+            // 
+            this.chess8.BackColor = System.Drawing.Color.White;
+            this.chess8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("chess8.BackgroundImage")));
+            this.chess8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chess8.Location = new System.Drawing.Point(150, 95);
+            this.chess8.Name = "chess8";
+            this.chess8.Size = new System.Drawing.Size(48, 48);
+            this.chess8.TabIndex = 11;
+            this.chess8.TabStop = false;
+            // 
+            // chess7
+            // 
+            this.chess7.BackColor = System.Drawing.Color.White;
+            this.chess7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("chess7.BackgroundImage")));
+            this.chess7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chess7.Location = new System.Drawing.Point(151, 166);
+            this.chess7.Name = "chess7";
+            this.chess7.Size = new System.Drawing.Size(48, 48);
+            this.chess7.TabIndex = 12;
+            this.chess7.TabStop = false;
+            // 
+            // chess6
+            // 
+            this.chess6.BackColor = System.Drawing.Color.White;
+            this.chess6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("chess6.BackgroundImage")));
+            this.chess6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chess6.Location = new System.Drawing.Point(215, 166);
+            this.chess6.Name = "chess6";
+            this.chess6.Size = new System.Drawing.Size(48, 48);
+            this.chess6.TabIndex = 13;
+            this.chess6.TabStop = false;
+            // 
+            // chess5
+            // 
+            this.chess5.BackColor = System.Drawing.Color.White;
+            this.chess5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("chess5.BackgroundImage")));
+            this.chess5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.chess5.Location = new System.Drawing.Point(276, 166);
+            this.chess5.Name = "chess5";
+            this.chess5.Size = new System.Drawing.Size(48, 48);
+            this.chess5.TabIndex = 14;
+            this.chess5.TabStop = false;
+            // 
             // GameRiver
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(494, 243);
+            this.Controls.Add(this.chess5);
+            this.Controls.Add(this.chess6);
+            this.Controls.Add(this.chess7);
+            this.Controls.Add(this.chess8);
+            this.Controls.Add(this.chess4);
+            this.Controls.Add(this.chess3);
+            this.Controls.Add(this.chess2);
+            this.Controls.Add(this.chess1);
+            this.Controls.Add(this.gameBack);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.messageList);
-            this.Controls.Add(this.panel1);
             this.Controls.Add(this.mainMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MainMenuStrip = this.mainMain;
             this.Name = "GameRiver";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "GameRiver";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.GameRiver_FormClosing);
             this.Load += new System.EventHandler(this.GameRiver_Load);
             this.mainMain.ResumeLayout(false);
             this.mainMain.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gameBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chess5)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,11 +287,19 @@
         private System.Windows.Forms.ToolStripMenuItem gameMenuItem;
         private System.Windows.Forms.ToolStripMenuItem startNewItem;
         private System.Windows.Forms.ToolStripMenuItem exitItem;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.ListBox messageList;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel statueBar;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox gameBack;
+        private System.Windows.Forms.PictureBox chess1;
+        private System.Windows.Forms.PictureBox chess2;
+        private System.Windows.Forms.PictureBox chess3;
+        private System.Windows.Forms.PictureBox chess4;
+        private System.Windows.Forms.PictureBox chess8;
+        private System.Windows.Forms.PictureBox chess7;
+        private System.Windows.Forms.PictureBox chess6;
+        private System.Windows.Forms.PictureBox chess5;
     }
 }
--- ZjSmallGameCollection/GameRiver.cs
+++ ZjSmallGameCollection/GameRiver.cs
@@ -13,29 +13,284 @@ namespace ZjSmallGameCollection
 {
     public partial class GameRiver : Form
     {
-        IGameRiverPlayer player;
+        private IGameRiverPlayer player;
+        private PictureBox[] pics;
+        private int[] chesses;
+        private int[] lastChesses;
+        private Thread mainUIThread;
+        private bool mainUIThreadEnd;
         public GameRiver()
         {
             InitializeComponent();
+        }
+
+        /// <summary>
+        /// 选定棋子，使其反白，若已选定，则取消
+        /// </summary>
+        /// <param name="pos">选定的棋子位置</param>
+        public void SelectChess(int pos)
+        {
+            if (this[pos] == 1 || this[pos] == -1)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    if (chesses[i] > 1)
+                        chesses[i] = 1;
+                    else if (chesses[i] < -1)
+                        chesses[i] = -1;
+                }
+                chesses[pos] *= 2;
+            }
+            else if (this[pos] != 0)
+            {
+                chesses[pos] /= 2;
+            }
+        }
+        /// <summary>
+        /// 检测两个位置是否相邻
+        /// </summary>
+        /// <param name="self">位置1</param>
+        /// <param name="target">位置2</param>
+        /// <returns>若相邻则返回true</returns>
+        public bool IsNearly(int self,int target)
+        {
+            if(self > 7 || self < 0 || target > 7 || target < 0 )
+                throw new System.Exception("Wrong index number of chesses");
+            if (self == target)
+                return false;
+            if (((self == 0 || self == 1) && target == 7) || (self == 7 && (target == 0 || target == 1)))
+                return true;
+            if (self - target == 1 || self - target == -1)
+                return true;
+            if (self / 2 == 0 && (self - target == 2 || self - target == -2))
+                return true;
+            return false;
+        }
+        /// <summary>
+        /// 获取指定位置的所有相邻位置
+        /// </summary>
+        /// <param name="self">指定的位置</param>
+        /// <param name="ignoredPos">要忽略的位置</param>
+        /// <returns>返回self的所有邻近的位置</returns>
+        public int[] GetAllNearIndex(int self, int ignoredPos = -1)
+        {
+            if (self > 7 || self < 0)
+                throw new System.Exception("Wrong index number of chesses");
+            var num = new int[] { -1, -1, -1, -1 };
+            int index = 0;
+            for(int i = 0; i < 8; i++)
+            {
+                if (IsNearly(self, i) && i != ignoredPos)
+                    num[index++] = i;
+            }
+            var res = new int[index];
+            for(int i = 0; i < index; i++)
+            {
+                res[i] = num[i];
+            }
+            return res;
+        }
+        /// <summary>
+        /// 检测是否能到达目标位置
+        /// </summary>
+        /// <param name="self">原位置</param>
+        /// <param name="target">要到达的目标位置</param>
+        /// <returns>如果目标位置与原位置相邻，且目标位置为空，则允许到达，返回true</returns>
+        public bool IsAbleToGo(int self, int target)
+        {
+            if (!IsNearly(self, target))
+                return false;
+            if (chesses[target] != 0)
+                return false;
+            return true;
+        }
+        /// <summary>
+        /// 获取所有可到达的位置
+        /// </summary>
+        /// <param name="self">原位置</param>
+        /// <param name="withPartner">是否将相邻的同阵营棋子算入列表</param>
+        /// <param name="ignoredPos">要忽略的位置</param>
+        /// <returns>返回结果</returns>
+        public int[] GetAllAbleIndex(int self, bool withPartner = false, int ignoredPos = -1)
+        {
+            var src = GetAllNearIndex(self, ignoredPos);
+            var index = src.Length;
+            for (int i = 0; i < src.Length; i++)
+            {
+                if ((!withPartner && chesses[src[i]] != 0) || (withPartner && chesses[src[i]] * self <= 0))
+                {
+                    index--;
+                    src[i] = -1;
+                }
+            }
+            if (index <= 0)
+                return null;
+
+            var res = new int[index];
+            for (int i = 0, j = 0; i < index; j++)
+            {
+                if (src[j] >= 0)
+                {
+                    res[i] = src[j];
+                    i++;
+                }
+            }
+            return res;
+        }
+        /// <summary>
+        /// 按此走棋是否将会吃掉敌方棋子
+        /// </summary>
+        /// <param name="src">原棋子位置</param>
+        /// <param name="dest">要走到的位置</param>
+        /// <returns>会吃掉敌方棋子，返回true</returns>
+        public bool WillCutEnemy(int src, int dest)
+        {
+            if (!IsAbleToGo(src, dest))
+                throw new System.Exception("Cannot go to here");
+            var nrly = GetAllNearIndex(dest, src);
+            for(int i = 0; nrly != null && i < nrly.Length; i++)
+            {
+                var res = GetAllAbleIndex(nrly[i], true, src);
+                if (nrly[i] != 0 && null == res)
+                    return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// 游戏结束判定
+        /// </summary>
+        /// <param name="src">要走的下一步棋子原位置</param>
+        /// <param name="dest">要走的下一步目标位置</param>
+        /// <returns>即将结束，返回true</returns>
+        public bool WillEndGame(int src = 0, int dest = 0)
+        {
+            if (Enemies == 0 || Ours == 0 || ((Enemies == 1 || Ours == 1) && WillCutEnemy(src, dest)))
+                return true;
+            return false;
+        }
+        /// <summary>
+        /// 取对应索引处的棋子状态
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public int this[int index]
+        {
+            get
+            {
+                if (index > 7 || index < 0)
+                    throw new System.Exception("Wrong index number of chesses");
+                return chesses[index];
+            }
+        }
+        /// <summary>
+        /// 获取或设定当前选中的棋子
+        /// </summary>
+        public int SelectedItem
+        {
+            get
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    if (chesses[i] > 1 || chesses[i] < -1)
+                        return i;
+                }
+                return -1;
+            }
+            set
+            {
+                if (value != SelectedItem)
+                    SelectChess(value);
+            }
+        }
+        /// <summary>
+        /// 获取敌人（黑方）剩余的棋子总数
+        /// </summary>
+        public int Enemies
+        {
+            get
+            {
+                int res = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    if (chesses[i] < 0)
+                        res++;
+                }
+                return res;
+            }
+        }
+        /// <summary>
+        /// 获取我方（红方）剩余的棋子总数
+        /// </summary>
+        public int Ours
+        {
+            get
+            {
+                int res = 0;
+                for (int i = 0; i < 8; i++)
+                {
+                    if (chesses[i] > 0)
+                        res++;
+                }
+                return res;
+            }
+        }
+        /// <summary>
+        /// 弹出过河棋游戏对话框，并隐藏父对话框
+        /// </summary>
+        /// <param name="parent">父对话框</param>
+        /// <returns>对话框返回值，恒为OK</returns>
+        public static DialogResult ShowDialog(Form parent)
+        {
+            parent.Hide();
+            var res = new GameRiver().ShowDialog((IWin32Window)parent);
+            parent.Show();
+            return res;
+        }
+
+        private void InitChesses()
+        {
+            chesses[0] = -1;
+            chesses[1] = -1;
+            chesses[2] = -1;
+            chesses[3] = 0;
+            chesses[4] = 0;
+            chesses[5] = 1;
+            chesses[6] = 1;
+            chesses[7] = 1;
         }
 
         private void GameRiver_Load(object sender, EventArgs e)
         {
             player = new GameRiver_Local(this);
             this.DialogResult = DialogResult.OK;
-        }
-
-        public static DialogResult ShowDialog(Form parent)
-        {
-            parent.Hide();
-            DialogResult res = new GameRiver().ShowDialog((IWin32Window)parent);
-            parent.Show();
-            return res;
+            pics = new PictureBox[8];
+            pics[0] = chess1;
+            pics[1] = chess2;
+            pics[2] = chess3;
+            pics[3] = chess4;
+            pics[4] = chess5;
+            pics[5] = chess6;
+            pics[6] = chess7;
+            pics[7] = chess8;
+            chesses = new int[8];
+            lastChesses = new int[8];
+            lastChesses[0] = 3;
+            lastChesses[1] = 3;
+            lastChesses[2] = 3;
+            lastChesses[3] = 3;
+            lastChesses[4] = 3;
+            lastChesses[5] = 3;
+            lastChesses[6] = 3;
+            lastChesses[7] = 3;
+            InitChesses();
+            mainUIThread = new Thread(mainUIThreadFunc);
+            mainUIThreadEnd = false;
+            mainUIThread.Start();
         }
 
         private void gameMenuItem_ChildClick(object sender, EventArgs e)
         {
-            ToolStripMenuItem sdr = (ToolStripMenuItem)sender;
+            var sdr = (ToolStripMenuItem)sender;
             switch(sdr.Name)
             {
                 case "startNewItem":
@@ -45,6 +300,46 @@ namespace ZjSmallGameCollection
                     this.Close();
                     break;
             }
+        }
+
+        private void mainUIThreadFunc()
+        {
+            var resources = new System.ComponentModel.ComponentResourceManager(typeof(GameRiver));
+            while (!mainUIThreadEnd)
+            {
+                Thread.Sleep(1);
+                for(int i = 0; i < 8; i++)
+                {
+                    if (chesses[i] != lastChesses[i])
+                    {
+                        lastChesses[i] = chesses[i];
+                        switch (chesses[i])
+                        {
+                            case -2:
+                                pics[i].Image = ((System.Drawing.Image)(resources.GetObject("chess2.Image")));
+                                break;
+                            case -1:
+                                pics[i].Image = ((System.Drawing.Image)(resources.GetObject("chess1.Image")));
+                                break;
+                            case 0:
+                                pics[i].Image = null;
+                                break;
+                            case 1:
+                                pics[i].Image = ((System.Drawing.Image)(resources.GetObject("chess7.Image")));
+                                break;
+                            case 2:
+                                pics[i].Image = ((System.Drawing.Image)(resources.GetObject("chess6.Image")));
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GameRiver_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            mainUIThreadEnd = true;
+            mainUIThread.Join();
         }
     }
 
